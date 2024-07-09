8a0d27922de66ba44450aa9591ef706551ddeb5e
Guillaume <guillaume@solidy.me>
2024-07-09 08:48:56 +0200

    initial architecture

---
+++ apps/api/src/app.ts
@@ -0,0 +1,43 @@
+import express, { NextFunction, Request, Response } from 'express'
+import { config as dotenvConfig } from 'dotenv'
+import cors from 'cors'
+
+dotenvConfig()
+
+/* --------------------------- Set up Application --------------------------- */
+const app = express();
+const port = process.env.PORT ?? 3000
+
+/**
+ * Body parser
+ */
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded())
+
+/**
+ * CORS
+ */
+app.set('trust proxy', 1)
+app.use(cors({
+	origin: process.env.FRONT_URL
+}))
+
+
+/* ----------------------------- Defines Routes ----------------------------- */
+app.get('/', (_req: Request, res: Response) => {
+	res.send('Hello world!')
+})
+
+
+/* --------------------------- Error Handling ------------------------------ */
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+	console.error(err.stack)
+
+	return res.status(500).send('Something went wrong')
+})
+
+
+/* ---------------------------- Start Application ---------------------------- */
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`)
+})
